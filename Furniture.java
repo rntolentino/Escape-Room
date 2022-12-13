92b5a442319b3689ab2e2350ae03f459a044c20c
tolentinoo <rtolentino@smith.edu>
2022-12-12 21:11:34 -0500

    fixed directions to player

--- Furniture.java
+++ Furniture.java
@@ -1,32 +1,45 @@
-// import java.io.*;
-// import java.util.*;
+import java.io.*;
+import java.util.*;
 
-// public class Furniture {
-//   public String name;  // can make private and use accessors and manipulators y
-//   private int locationX;
-//   private int locationY;
-//   public static void main(String args[])
-//     {
-//         Hashtable<Integer, String> ht1 = new Hashtable<>();
-  
-//         // Initialization of a Hashtable
-//         Hashtable<Integer, String> ht2
-//             = new Hashtable<Integer, String>();
-//     }
-//   /**
-// the goal for this section is to create a memory area for the items to be stored and remembered so that they can then be added to inventory. Once again, not completely sure on how to execute this. 
-//       **/
+public class Furniture {
+    public String name;  // can make private and use accessors and manipulators y
+    private String location_X;
+    private String location_Y;
+    private String storage;
+    
+    public Furniture (String name, int location_x, int location_y) {
+        this.name = name;
+        // this.location_X = location_x;
+        // this.location_Y= location_y;
+        
+      }
+    /**
+    the goal for this section is to create a memory area for the items to be stored and remembered so that 
+    they can then be added to inventory. Once again, not completely sure on how to execute this. 
+    **/
 
-// // methods
-//   public String moveDirections(int locationX, int locationY){
-//     /**
-// the goal here is to be able to give location to the items in item.java in the escape room. This method is a bit confusing since we would probably need to do some inheritance or something that'd allow us to connect the classes alltogether. 
-//     **/
-//   }
+    // methods
+    /**
+    the goal here is to be able to give location to the items in item.java in the escape room.
+    This method is a bit confusing since we would probably need to do some inheritance or something 
+    that'd allow us to connect the classes alltogether. 
+    **/
+    public void directions(String locationX, String locationY){
+        if(this.name.toLowerCase()== "computer"|| this.name.toLowerCase()== "desk"){
+            this.location_X = "0" ;
+            this.location_Y= "north";
+        }
+        if (this.name.toLowerCase()==" "){
+            
+        }
+        }
 
-//   public String liftItems(String Items, String storage){
-// /**
-// maybe this will not be used since there is already a grab method in Player.java
-//     **/
-//   }
-// }
+        
+    }
+    
+ 
+  }
+  public static void main(String args[]){
+        
+    }
+}
