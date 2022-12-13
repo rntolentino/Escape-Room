8a203d092e3559ea52f24e255290fb712e2a1dff
tolentinoo <rtolentino@smith.edu>
2022-12-12 21:40:20 -0500

    fixed furniture

--- Furniture.java
+++ Furniture.java
@@ -13,6 +13,11 @@ public class Furniture {
         // this.location_Y= location_y;
         
       }
+
+    public String getLocation_X(){
+      return (this.location_X );
+    } 
+    
     /**
     the goal for this section is to create a memory area for the items to be stored and remembered so that 
     they can then be added to inventory. Once again, not completely sure on how to execute this. 
@@ -25,20 +30,31 @@ public class Furniture {
     that'd allow us to connect the classes alltogether. 
     **/
     public void directions(String locationX, String locationY){
-        if(this.name.toLowerCase()== "computer"|| this.name.toLowerCase()== "desk"){
-            this.location_X = "0" ;
-            this.location_Y= "north";
+        if(this.name.toLowerCase()=="door"){
+            this.location_X = "north" ;
+            this.location_Y= "0";
         }
-        if (this.name.toLowerCase()==" "){
-            
+        if (this.name.toLowerCase()=="bookcase" ){
+          this.location_X = "0" ;
+          this.location_Y= "north";
+        }
+        if (this.name.toLowerCase()=="window" ){
+          this.location_X = "east" ;
+          this.location_Y= "0";
+
+        }
+        if (this.name.toLowerCase()== "lamp"|| this.name.toLowerCase()== "desk"){
+          this.location_X = "0" ;
+          this.location_Y= "west";
         }
         }
 
+
         
-    }
+    
     
  
-  }
+  
   public static void main(String args[]){
         
     }
